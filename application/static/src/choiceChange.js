8ac143105b33761080a80a2e0b1af56020f3c10a
Alex <gh@cld.tushynski.me>
2023-02-07 22:05:57 +0000

    hotfix

--- application/static/src/choiceChange.js
+++ application/static/src/choiceChange.js
@@ -1,17 +1,3 @@
-
-function docsIndex() {
-    // loads latest index from https://raw.githubusercontent.com/arc53/DocsHUB/main/combined.json
-    // and stores it in localStorage
-    fetch('https://raw.githubusercontent.com/arc53/DocsHUB/main/combined.json')
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-            localStorage.setItem("docsIndex", JSON.stringify(data));
-        }
-    )
-
-}
-
 document.getElementById("select-docs").addEventListener("change", function() {
 localStorage.setItem('activeDocs', this.value)
      fetch('/api/docs_check', {
