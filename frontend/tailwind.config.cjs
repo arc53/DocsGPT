3aed55ba60382896f5b89159e9d4dbbb18ce0545
TaylorS15 <taylorsvec978@yahoo.com>
2023-02-10 16:20:30 -0500

    extended spacing

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        112: '28rem',
+        128: '32rem',
+      },
+    },
   },
   plugins: [],
 }
