1d3e5b85fc12231623ee2e112947f3d56a01eaf5
TaylorS15 <taylorsvec978@yahoo.com>
2023-02-11 11:56:44 -0500

    react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
 import DocsGPT from './components/DocsGPT';
+import APIKeyModal from './components/APIKeyModal';
 import './App.css';
 
-function App() {
+export default function App() {
+  //Currently using primitive state management. Will most likely be replaced with Redux.
   const [isMobile, setIsMobile] = useState(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+  const [isApiModalOpen, setIsApiModalOpen] = useState(true);
+  const [apiKey, setApiKey] = useState('');
 
   const handleResize = () => {
     if (window.innerWidth > 768 && isMobile) {
@@ -24,11 +30,26 @@ function App() {
   }, []);
 
   return (
-    <div className={`${isMobile ? 'flex-col' : 'flex-row'} flex`}>
-      <Navigation isMobile={isMobile} />
-      <DocsGPT />
+    <div
+      className={`${
+        isMobile ? 'flex-col' : 'flex-row'
+      } relative flex transition-all`}
+    >
+      <APIKeyModal
+        apiKey={apiKey}
+        setApiKey={setApiKey}
+        isApiModalOpen={isApiModalOpen}
+        setIsApiModalOpen={setIsApiModalOpen}
+      />
+      <Navigation
+        isMobile={isMobile}
+        isMenuOpen={isMenuOpen}
+        setIsMenuOpen={setIsMenuOpen}
+        setIsApiModalOpen={setIsApiModalOpen}
+      />
+      <Routes>
+        <Route path="/" element={<DocsGPT isMenuOpen={isMenuOpen} />} />
+      </Routes>
     </div>
   );
 }
-
-export default App;
--- frontend/src/components/DocsGPT.tsx
+++ frontend/src/components/DocsGPT.tsx
@@ -1,3 +1,7 @@
-export default function DocsGPT() {
-  return <div className="md:ml-72 lg:ml-96">Docs GPT Chat Placeholder</div>;
+export default function DocsGPT({ isMenuOpen }: { isMenuOpen: boolean }) {
+  return (
+    <div className={`${isMenuOpen ? 'md:ml-72 lg:ml-96' : 'ml-16'}`}>
+      Docs GPT Chat Placeholder
+    </div>
+  );
 }
