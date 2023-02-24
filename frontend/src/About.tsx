90ec40c2b301d25f50570e1e1a4811904dbcba16
Alex <a@tushynski.me>
2023-02-24 14:48:02 +0000

    spacing

--- frontend/src/About.tsx
+++ frontend/src/About.tsx
@@ -5,9 +5,9 @@ export default function About() {
   return (
     //Parent div for all content shown through App.tsx routing needs to have this styling. Might change when state management is updated.
     <div className="mx-36 grid min-h-screen">
-      <article className=" mx-auto my-auto flex w-full max-w-6xl flex-col place-items-center gap-6 rounded-lg bg-gray-100 p-6 text-jet lg:p-10 xl:p-16">
-        <p className="text-3xl font-semibold">About DocsGPT 🦖</p>
-        <p className="mt-4 text-xl">
+      <article className=" place-items-left mx-auto my-auto flex w-full max-w-6xl flex-col gap-6 rounded-lg bg-gray-100 p-6 text-jet lg:p-10 xl:p-16">
+        <p className="text-3xl">About DocsGPT 🦖</p>
+        <p className="mt-4">
           Find the information in your documentation through AI-powered
           <a
             className="text-blue-500"
@@ -22,33 +22,33 @@ export default function About() {
         </p>
 
         <div>
-          <p className="text-lg">
+          <p>
             If you want to add your own documentation, please follow the
             instruction below:
           </p>
-          <p className="mt-4 text-lg">
+          <p className="mt-4 ml-2">
             1. Navigate to{' '}
             <span className="bg-gray-200 italic"> /application</span> folder
           </p>
-          <p className="mt-4 text-lg">
+          <p className="mt-4 ml-2">
             2. Install dependencies from{' '}
             <span className="bg-gray-200 italic">
               pip install -r requirements.txt
             </span>
           </p>
-          <p className="mt-4 text-lg">
+          <p className="mt-4 ml-2">
             3. Prepare a <span className="bg-gray-200 italic">.env</span> file.
             Copy <span className="bg-gray-200 italic">.env_sample</span> and
             create <span className="bg-gray-200 italic">.env</span> with your
             OpenAI API token
           </p>
-          <p className="mt-4 text-lg">
+          <p className="mt-4 ml-2">
             4. Run the app with{' '}
             <span className="bg-gray-200 italic">python app.py</span>
           </p>
         </div>
 
-        <p className="text-lg">
+        <p>
           Currently It uses python pandas documentation, so it will respond to
           information relevant to pandas. If you want to train it on different
           documentation - please follow
@@ -64,7 +64,7 @@ export default function About() {
           .
         </p>
 
-        <p className="mt-4 text-left text-lg">
+        <p className="mt-4 text-left">
           If you want to launch it on your own server - follow
           <a
             className="text-blue-500"
