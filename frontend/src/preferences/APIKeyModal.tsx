01320798423db4b3f9583b15fb8ad9c759211aed
ajaythapliyal <ajaythapliyal1703@gmail.com>
2023-02-24 07:59:19 +0530

    fixes 1. from Nicks list

--- frontend/src/preferences/APIKeyModal.tsx
+++ frontend/src/preferences/APIKeyModal.tsx
@@ -41,7 +41,7 @@ export default function APIKeyModal({
     >
       <article className="mx-auto mt-24 flex w-[90vw] max-w-lg  flex-col gap-4 rounded-lg bg-white p-6 shadow-lg">
         <p className="text-xl text-jet">OpenAI API Key</p>
-        <p className="text-lg leading-5 text-gray-500">
+        <p className="text-md leading-6 text-gray-500">
           Before you can start using DocsGPT we need you to provide an API key
           for llm. Currently, we support only OpenAI but soon many more. You can
           find it here.
