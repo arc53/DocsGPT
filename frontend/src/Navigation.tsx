ac8dbf4109cc1b59ca2a421f217a7a894e480f91
ajaythapliyal <ajaythapliyal1703@gmail.com>
2023-02-28 09:27:22 +0530

    fix

--- frontend/src/Navigation.tsx
+++ frontend/src/Navigation.tsx
@@ -55,7 +55,7 @@ export default function Navigation({
         setIsDocsListOpen(false);
       }
     },
-    [isDocsListOpen, apiKeyModalState],
+    [navState, isDocsListOpen, apiKeyModalState],
   );
 
   return (
