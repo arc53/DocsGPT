0c1a6a918d80742117df3a2c8285de4e5eb33138
Alex <a@tushynski.me>
2023-04-23 15:07:55 +0100

    web widget

--- application/app.py
+++ application/app.py
@@ -94,7 +94,7 @@ app.config['CELERY_BROKER_URL'] = os.getenv("CELERY_BROKER_URL")
 app.config['CELERY_RESULT_BACKEND'] = os.getenv("CELERY_RESULT_BACKEND")
 app.config['MONGO_URI'] = os.getenv("MONGO_URI")
 celery = Celery()
-celery.config_from_object(celeryconfig)
+celery.config_from_object('celeryconfig')
 mongo = MongoClient(app.config['MONGO_URI'])
 db = mongo["docsgpt"]
 vectors_collection = db["vectors"]
