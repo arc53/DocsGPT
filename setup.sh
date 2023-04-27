0beafb8391fa9b8b46db9f409d5a3dae0bd4da60
Darth Pika <103236915+darth-pika-hu@users.noreply.github.com>
2023-04-27 12:39:03 -0700

    Update setup.sh
    This script includes the necessary changes to use container linking and updated environment variables for the `backend` and `worker` containers.
    
    Make sure you have the `./frontend` and `./application` directories in the correct locations before running the script.

--- setup.sh
+++ setup.sh
@@ -16,29 +16,30 @@ docker run -d --name mongo -p 27017:27017 -v mongodb_data_container:/data/db mon
 
 # Run backend and worker services
 docker run -d --name backend -p 5001:5001 \
+  --link redis:redis --link mongo:mongo \
   -v $(pwd)/application/indexes:/app/indexes \
   -v $(pwd)/application/inputs:/app/inputs \
   -v $(pwd)/application/vectors:/app/vectors \
   -e API_KEY=$OPENAI_API_KEY \
   -e EMBEDDINGS_KEY=$OPENAI_API_KEY \
-  -e CELERY_BROKER_URL=redis://localhost:6379/0 \
-  -e CELERY_RESULT_BACKEND=redis://localhost:6379/1 \
-  -e MONGO_URI=mongodb://localhost:27017/docsgpt \
+  -e CELERY_BROKER_URL=redis://redis:6379/0 \
+  -e CELERY_RESULT_BACKEND=redis://redis:6379/1 \
+  -e MONGO_URI=mongodb://mongo:27017/docsgpt \
   backend_image
 
 docker run -d --name worker \
+  --link redis:redis --link mongo:mongo \
   -e API_KEY=$OPENAI_API_KEY \
   -e EMBEDDINGS_KEY=$OPENAI_API_KEY \
-  -e CELERY_BROKER_URL=redis://localhost:6379/0 \
-  -e CELERY_RESULT_BACKEND=redis://localhost:6379/1 \
-  -e MONGO_URI=mongodb://localhost:27017/docsgpt \
-  -e API_URL=http://localhost:5001 \
-  backend_image celery -A app.celery worker -l INFO
+  -e CELERY_BROKER_URL=redis://redis:6379/0 \
+  -e CELERY_RESULT_BACKEND=redis://redis:6379/1 \
+  -e MONGO_URI=mongodb://mongo:27017/docsgpt \
+  -e API_URL=http://backend:5001 \
+  backend_image \
+  celery -A app.celery worker -l INFO
 
 # Run frontend service
 docker run -d --name frontend -p 5173:5173 \
   -e VITE_API_HOST=http://localhost:5001 \
   frontend_image
 
-# List running containers
-docker ps
