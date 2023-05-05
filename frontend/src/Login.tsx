ac4e7414c6282d0d514f196ea1d096a15b2c1a85
Leanne Holmes <lholmes24@my.bcit.ca>
2023-05-04 22:19:13 -0700

    Updated Login page design

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -5,13 +5,13 @@ export default function Login() {
     <>
       {' '}
       <section className="gradient-form h-full justify-center bg-neutral-100 dark:bg-neutral-700">
-        <div className="container mx-auto h-full justify-center p-10">
+        <div className="container mx-auto h-full w-7/12 justify-center p-10">
           <div className="g-6 flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
             <div className="w-full">
               <div className="block rounded-lg bg-white shadow-lg dark:bg-neutral-800 ">
                 <div className="g-0 lg:flex lg:flex-wrap">
-                  <div className="flex items-center bg-neutral-200 lg:w-6/12 lg:rounded-bl-none">
-                    <div className="px-4 py-6 text-black md:mx-6 md:p-12">
+                  <div className="loginAbout flex items-center lg:w-5/12 lg:rounded-bl-none">
+                    {/* <div className="px-4 py-6 text-black md:mx-6 md:p-12">
                       <h4 className="mb-6 text-xl font-semibold">
                         Document Genius
                       </h4>
@@ -22,57 +22,65 @@ export default function Login() {
                         nostrud exercitation ullamco laboris nisi ut aliquip ex
                         ea commodo consequat.
                       </p>
-                    </div>
+                    </div> */}
                   </div>
-                  <div className="px-4 md:px-0 lg:w-6/12">
+                  <div className="justify-center px-4 md:px-0 lg:w-7/12">
                     <div className="md:mx-6 md:p-12">
                       <div className="text-center">
-                        <h4 className="mb-12 mt-1 pb-1 text-xl font-semibold">
-                          Welcome to Document Genius!
+                        <h4 className="loginTitle mb-8 mt-10 pb-1 font-semibold">
+                          Document Genius
                         </h4>
                       </div>
 
+                      <p className="text-md mb-8">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex
+                        ea commodo consequat.
+                      </p>
+                      <hr></hr>
+
                       <form>
-                        <p className="mb-4">Please login to your account</p>
+                        <p className="text-md mb-4 mt-8">
+                          Login to your account
+                        </p>
                         <div
                           className="relative mb-4"
                           data-te-input-wrapper-init
                         >
-                          <input
-                            type="text"
-                            className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                            id="exampleFormControlInput1"
-                            placeholder="Username"
-                          />
-                          <label
-                            htmlFor="exampleFormControlInput1"
-                            className="peer-focus:text-primary dark:peer-focus:text-primary pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200"
+                          <div
+                            className="relative mb-5"
+                            data-te-input-wrapper-init
                           >
-                            Username
-                          </label>
+                            <input
+                              className="inputText peer block min-h-[auto] w-full rounded border-2 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none focus:placeholder:opacity-100"
+                              id="userName"
+                              type="text"
+                              placeholder="Username"
+                            />
+                          </div>
+                          <div
+                            className="relative mb-5"
+                            data-te-input-wrapper-init
+                          >
+                            <input
+                              className="inputText peer block min-h-[auto] w-full rounded border-2 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none focus:placeholder:opacity-100"
+                              id="userPassword"
+                              type="password"
+                              placeholder="Password"
+                            />
+                          </div>
                         </div>
                         <div
                           className="relative mb-4"
                           data-te-input-wrapper-init
-                        >
-                          <input
-                            type="password"
-                            className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                            id="exampleFormControlInput11"
-                            placeholder="Password"
-                          />
-                          <label
-                            htmlFor="exampleFormControlInput11"
-                            className="peer-focus:text-primary dark:peer-focus:text-primary pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200"
-                          >
-                            Password
-                          </label>
-                        </div>
+                        ></div>
 
                         <div className="mb-12 pb-1 pt-1 text-center">
                           <Link to="/query">
                             <button
-                              className="text-grey mb-3 inline-block w-full rounded bg-blue-200 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
+                              className="loginButton mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
                               type="button"
                               data-te-ripple-init
                               data-te-ripple-color="light"
@@ -81,7 +89,7 @@ export default function Login() {
                             </button>
                           </Link>
 
-                          <span className="text-sm">
+                          <span className="forgotPassword text-sm">
                             <a href="#!">Forgot password?</a>
                           </span>
                         </div>
@@ -91,8 +99,6 @@ export default function Login() {
                           <button
                             type="button"
                             className="border-danger text-danger hover:border-danger-600 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 active:border-danger-700 active:text-danger-700 inline-block rounded border-2 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal transition duration-150 ease-in-out hover:bg-neutral-500 hover:bg-opacity-10 focus:outline-none focus:ring-0 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
-                            data-te-ripple-init
-                            data-te-ripple-color="light"
                           >
                             Register
                           </button>
