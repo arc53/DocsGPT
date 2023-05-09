4d5032f4f629bdadb3e6f750ce8126e861e01052
Leanne Holmes <lholmes24@my.bcit.ca>
2023-05-08 23:04:48 -0700

    Added resizable div

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,6 +17,59 @@ export default function App() {
     window.matchMedia('(min-width: 768px)').matches ? 'ACTIVE' : 'INACTIVE',
   );
 
+  const resizer = document.getElementById('dragMe');
+  if (resizer) {
+    const leftSide = resizer.previousElementSibling;
+    const rightSide = resizer.nextElementSibling;
+    let x = 0;
+    let y = 0;
+    let leftWidth = 0;
+    const mouseDownHandler = function (e) {
+      // Get the current mouse position
+      x = e.clientX;
+      y = e.clientY;
+      if (leftSide) {
+        leftWidth = leftSide.getBoundingClientRect().width;
+      }
+      // Attach the listeners to `document`
+      document.addEventListener('mousemove', mouseMoveHandler);
+      document.addEventListener('mouseup', mouseUpHandler);
+    };
+    resizer.addEventListener('mousedown', mouseDownHandler);
+
+    const mouseMoveHandler = function (e) {
+      // How far the mouse has been moved
+      const dx = e.clientX - x;
+      const dy = e.clientY - y;
+      if (resizer && resizer.parentElement) {
+        const newLeftWidth =
+          ((leftWidth + dx) * 100) /
+          resizer.parentElement.getBoundingClientRect().width;
+        if (leftSide) {
+          leftSide.style.width = `${newLeftWidth}%`;
+        }
+      }
+    };
+
+    const mouseUpHandler = function () {
+      resizer.style.removeProperty('cursor');
+      document.body.style.removeProperty('cursor');
+      if (leftSide) {
+        leftSide.style.removeProperty('user-select');
+        leftSide.style.removeProperty('pointer-events');
+      }
+
+      if (rightSide) {
+        rightSide.style.removeProperty('user-select');
+        rightSide.style.removeProperty('pointer-events');
+      }
+
+      // Remove the handlers of `mousemove` and `mouseup`
+      document.removeEventListener('mousemove', mouseMoveHandler);
+      document.removeEventListener('mouseup', mouseUpHandler);
+    };
+  }
+
   return (
     <>
       <Routes>
@@ -29,6 +82,7 @@ export default function App() {
               <div className="docNavigation">
                 <DocNavigation />
               </div>
+              <div className="resizer" id="dragMe"></div>
               <div className="docWindow">
                 <DocWindow />
               </div>
