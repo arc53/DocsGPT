3c88d50794234cb7370d3ac3aaffeeb35e7962de
Ray Zhang <rzhang104@my.bcit.ca>
2023-05-08 23:09:58 -0700

    begun changes to greetings message on login

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -56,11 +56,7 @@ export default function Login() {
                       </div>
 
                       <p className="text-md mb-8">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit, sed do eiusmod tempor incididunt ut labore et
-                        dolore magna aliqua. Ut enim ad minim veniam, quis
-                        nostrud exercitation ullamco laboris nisi ut aliquip ex
-                        ea commodo consequat.
+                        Welcome to Document Genius!
                       </p>
                       <hr></hr>
                       <div id="loginForm">
