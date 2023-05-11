ed0585cb1f202da11b065e8003bcd2d4ad542bd7
Leanne Holmes <lholmes24@my.bcit.ca>
2023-05-10 19:55:08 -0700

    Reverted back to grid layout, fixed conversation div overflow

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,6 @@ import { ActiveState } from './models/misc';
 import { inject } from '@vercel/analytics';
 import Login from './Login';
 import Register from './Register';
-import Draggable from 'react-draggable';
 
 inject();
 
@@ -17,35 +16,6 @@ export default function App() {
   const [navState, setNavState] = useState<ActiveState>(
     window.matchMedia('(min-width: 768px)').matches ? 'ACTIVE' : 'INACTIVE',
   );
-
-  const [widths, setWidths] = useState([15, 35, 35, 15]);
-
-  const [startX, setStartX] = useState(0); // [startX, setStartX
-  const totalWidth = window.innerWidth;
-
-  const handleDragStart = (event: any) => {
-    setStartX(event.clientX);
-  };
-
-  const handleDrag = (index: number, event: any) => {
-    if (index == 3) {
-      return;
-    } // do nothing with the last division.
-
-    const preWidthforCurrent = widths[index];
-    const preWidthforNextOne = widths[index + 1];
-    const pixelDiff = event.clientX - startX;
-    const diff = (pixelDiff / totalWidth) * 100;
-    console.log('diff: ' + diff);
-
-    const newWidths = [...widths];
-    newWidths[index] = preWidthforCurrent + (diff / totalWidth) * 100;
-    newWidths[index + 1] = preWidthforNextOne - (diff / totalWidth) * 100;
-    if (newWidths[3] < 15) {
-      return;
-    }
-    setWidths(newWidths);
-  };
 
   return (
     <>
@@ -56,32 +26,19 @@ export default function App() {
           path="/query"
           element={
             <div className="wrapper">
-              {widths.map((width, index) => (
-                <Draggable
-                  axis="x"
-                  bounds={{ right: window.innerWidth }}
-                  position={{ x: widths[index], y: 0 }}
-                  onStart={(e: any) => handleDragStart(e)}
-                  onDrag={(e: any) => handleDrag(index, e)}
-                  key={index}
-                >
-                  <div
-                    style={{
-                      width: `${width}%`,
-                    }}
-                  >
-                    {index === 0 && <DocNavigation />}
-                    {index === 1 && <DocWindow />}
-                    {index === 2 && <Conversation />}
-                    {index === 3 && (
-                      <Navigation
-                        navState={navState}
-                        setNavState={setNavState}
-                      />
-                    )}
-                  </div>
-                </Draggable>
-              ))}
+              <div className="docNavigation">
+                <DocNavigation />
+              </div>
+              <div className="docWindow">
+                <DocWindow />
+              </div>
+              <div className="chatWindow">
+                <Conversation />
+              </div>
+              <div className="chatNavigation">
+                {' '}
+                <Navigation navState={navState} setNavState={setNavState} />
+              </div>
             </div>
           }
         />
