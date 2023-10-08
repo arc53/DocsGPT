01457bbe79750130556752aadf2009cbc78075c5
Akshay <akshay.avinash@sap.com>
2023-10-08 20:05:08 +0530

    Add feedback endpoint

--- mock-backend/src/server.js
+++ mock-backend/src/server.js
@@ -1,26 +1,31 @@
-import jsonServer from 'json-server';
-import routes from './mocks/routes.json' assert { type: "json" };
-
+import jsonServer from "json-server";
+import routes from "./mocks/routes.json" assert { type: "json" };
 
 const server = jsonServer.create();
-const router = jsonServer.router('./src/mocks/db.json');
+const router = jsonServer.router("./src/mocks/db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 
 server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    if (req.url.includes('/delete_conversation')) {
-      req.method = 'DELETE';
+  if (req.method === "POST") {
+    if (req.url.includes("/delete_conversation")) {
+      req.method = "DELETE";
     }
   }
-  next()
-})
+  next();
+});
 
 server.use(jsonServer.rewriter(routes));
+
+router.render = (req, res) => {
+  if (req.url === "/feedback") {
+    res.status(200).jsonp({ status: "ok" });
+  }
+};
 
 server.use(router);
 
 server.listen(7091, () => {
-  console.log('JSON Server is running')
+  console.log("JSON Server is running");
 });
