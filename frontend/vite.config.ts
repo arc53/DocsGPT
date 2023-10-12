e1124711730f4d68df18d61e2a65098b14b9b3b4
Ezra Adeyinka <adeyinkaezra123@gmail.com>
2023-10-12 04:40:08 +0100

    refactor(frontend): modify config files for module alias path imports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 });
