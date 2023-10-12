993c9b31bddffddcd68b1260432e65c4fd5925c1
M Sai Kiran <116418856+MSaiKiran9@users.noreply.github.com>
2023-10-12 15:10:26 +0530

    Update Upload.tsx

--- frontend/src/upload/Upload.tsx
+++ frontend/src/upload/Upload.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { useCallback, useEffect, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { useDispatch } from 'react-redux';
