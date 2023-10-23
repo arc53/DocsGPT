4086649b6e0dded89071e0d93b1e63c179251e31
akshayram1 <akshaychame2@gmail.com>
2023-10-23 13:15:54 +0530

    Update Tailwind CSS colors in configuration

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -10,15 +10,13 @@ module.exports = {
       },
       colors: {
         'eerie-black': '#212121',
-        'black-1000': '#343541',
+        'custom-black': '#000000', // Change the color 'black-1000' to pure black
         jet: '#343541',
         'gray-alpha': 'rgba(0,0,0, .1)',
-        'gray-1000': '#F6F6F6',
+        'gray-1000': '#F6F6F6', // Change the color 'gray-1000'
         'gray-2000': 'rgba(0, 0, 0, 0.5)',
         'gray-3000': 'rgba(243, 243, 243, 1)',
-        'gray-4000': '#949494',
-        'gray-5000': '#BBBBBB',
-        'gray-6000': '#757575',
+        'custom-gray': '#888888', // Add a custom gray color
         'red-1000': 'rgb(254, 202, 202)',
         'red-2000': '#F44336',
         'red-3000': '#621B16',
@@ -27,6 +25,7 @@ module.exports = {
         'blue-3000': '#4B02E2',
         'purple-30': '#7D54D1',
         'blue-4000': 'rgba(0, 125, 255, 0.36)',
+        'custom-blue': '#0088CC', // Add a custom blue color
         'blue-5000': 'rgba(0, 125, 255)',
       },
     },
