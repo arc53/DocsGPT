6e131e04eb12c35dc85d55788fe6ad7cda4869b3
Saksham Tyagi <sakshamtyagi2008@gmail.com>
2023-10-31 16:31:18 +0530

    cofig

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -6,7 +6,6 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}',
   "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
   darkMode: 'class',
-  plugins: [nextui()],
   theme: {
     extend: {
       spacing: {
@@ -42,5 +41,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 };
