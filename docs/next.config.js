6db38ad7697a960a4bcce8253b0e77079f7e5e40
QuentiumYT <quentin.lienhardt@gmail.com>
2024-01-05 18:50:49 +0100

    Bump dependencies & support next 14 for docs
    - Renamed _app.js to mdx (for Next 14)
    - Lint next config file & package.json

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,9 +1,9 @@
 const withNextra = require('nextra')({
-    theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.jsx'
-  })
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx'
+})
+
+module.exports = withNextra()
    
-  module.exports = withNextra()
-   
-  // If you have other Next.js configurations, you can pass them as the parameter:
-  // module.exports = withNextra({ /* other next.js config */ })
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
