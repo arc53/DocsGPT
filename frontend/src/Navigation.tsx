51f588f4b1e784f05b5dbedf6cbb15c086a50063
Siddhant Rai <siddhant.rai.5686@gmail.com>
2024-03-04 15:45:34 +0530

    fix: issue #157

--- frontend/src/Navigation.tsx
+++ frontend/src/Navigation.tsx
@@ -7,9 +7,7 @@ import DocumentationDark from './assets/documentation-dark.svg';
 import Discord from './assets/discord.svg';
 import DiscordDark from './assets/discord-dark.svg';
 
-import Arrow2 from './assets/dropdown-arrow.svg';
 import Expand from './assets/expand.svg';
-import Trash from './assets/trash.svg';
 import Github from './assets/github.svg';
 import GithubDark from './assets/github-dark.svg';
 import Hamburger from './assets/hamburger.svg';
@@ -42,27 +40,23 @@ import { Doc, getConversations } from './preferences/preferenceApi';
 import SelectDocsModal from './preferences/SelectDocsModal';
 import ConversationTile from './conversation/ConversationTile';
 import { useDarkTheme } from './hooks';
+import Dropdown from './components/Dropdown';
 
 interface NavigationProps {
   navOpen: boolean;
   setNavOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
-const NavImage: React.FC<{ Light: string, Dark: string }> = ({ Light, Dark }) => {
+const NavImage: React.FC<{ Light: string; Dark: string }> = ({
+  Light,
+  Dark,
+}) => {
   return (
     <>
-      <img
-        src={Dark}
-        alt="icon"
-        className="ml-2 w-5 hidden dark:block "
-      />
-      <img
-        src={Light}
-        alt="icon"
-        className="ml-2 w-5 dark:hidden "
-      />
+      <img src={Dark} alt="icon" className="ml-2 hidden w-5 dark:block " />
+      <img src={Light} alt="icon" className="ml-2 w-5 dark:hidden " />
     </>
-  )
-}
+  );
+};
 export default function Navigation({ navOpen, setNavOpen }: NavigationProps) {
   const dispatch = useDispatch();
   const docs = useSelector(selectSourceDocs);
@@ -86,9 +80,6 @@ export default function Navigation({ navOpen, setNavOpen }: NavigationProps) {
 
   const navRef = useRef(null);
   const apiHost = import.meta.env.VITE_API_HOST || 'https://docsapi.arc53.com';
-  const embeddingsName =
-    import.meta.env.VITE_EMBEDDINGS_NAME ||
-    'huggingface_sentence-transformers/all-mpnet-base-v2';
 
   const navigate = useNavigate();
 
@@ -203,15 +194,17 @@ export default function Navigation({ navOpen, setNavOpen }: NavigationProps) {
           <img
             src={Expand}
             alt="menu toggle"
-            className={`${!navOpen ? 'rotate-180' : 'rotate-0'
-              } m-auto transition-all duration-200`}
+            className={`${
+              !navOpen ? 'rotate-180' : 'rotate-0'
+            } m-auto transition-all duration-200`}
           />
         </button>
       )}
       <div
         ref={navRef}
-        className={`${!navOpen && '-ml-96 md:-ml-[18rem]'
-          } duration-20 fixed top-0 z-20 flex h-full w-72 flex-col border-r-[1px] border-b-0 dark:border-r-purple-taupe bg-white dark:bg-chinese-black transition-all dark:text-white`}
+        className={`${
+          !navOpen && '-ml-96 md:-ml-[18rem]'
+        } duration-20 fixed top-0 z-20 flex h-full w-72 flex-col border-r-[1px] border-b-0 bg-white transition-all dark:border-r-purple-taupe dark:bg-chinese-black dark:text-white`}
       >
         <div
           className={'visible mt-2 flex h-[6vh] w-full justify-between md:h-12'}
@@ -229,8 +222,9 @@ export default function Navigation({ navOpen, setNavOpen }: NavigationProps) {
             <img
               src={Expand}
               alt="menu toggle"
-              className={`${!navOpen ? 'rotate-180' : 'rotate-0'
-                } m-auto transition-all duration-200`}
+              className={`${
+                !navOpen ? 'rotate-180' : 'rotate-0'
+              } m-auto transition-all duration-200`}
             />
           </button>
         </div>
@@ -245,8 +239,9 @@ export default function Navigation({ navOpen, setNavOpen }: NavigationProps) {
             );
           }}
           className={({ isActive }) =>
-            `${isActive ? 'bg-gray-3000 dark:bg-transparent' : ''
-            } group sticky mx-4 mt-4 flex cursor-pointer gap-2.5 rounded-3xl border border-silver p-3 hover:border-rainy-gray dark:border-purple-taupe dark:text-white dark:hover:bg-transparent hover:bg-gray-3000`
+            `${
+              isActive ? 'bg-gray-3000 dark:bg-transparent' : ''
+            } group sticky mx-4 mt-4 flex cursor-pointer gap-2.5 rounded-3xl border border-silver p-3 hover:border-rainy-gray hover:bg-gray-3000 dark:border-purple-taupe dark:text-white dark:hover:bg-transparent`
           }
         >
           <img
@@ -258,7 +253,7 @@ export default function Navigation({ navOpen, setNavOpen }: NavigationProps) {
             New Chat
           </p>
         </NavLink>
-        <div className="mb-auto h-[56vh] overflow-x-hidden dark:text-white overflow-y-scroll">
+        <div className="mb-auto h-[56vh] overflow-x-hidden overflow-y-scroll dark:text-white">
           {conversations && (
             <div>
               <p className="ml-6 mt-3 text-sm font-semibold">Chats</p>
@@ -282,88 +277,44 @@ export default function Navigation({ navOpen, setNavOpen }: NavigationProps) {
         <div className="flex h-auto flex-col justify-end text-eerie-black dark:text-white">
           <div className="flex flex-col-reverse border-b-[1px] dark:border-b-purple-taupe">
             <div className="relative my-4 flex gap-2 px-2">
-              <div
-                className="flex h-12 w-5/6 cursor-pointer justify-between rounded-3xl border-2 dark:border-chinese-silver bg-white dark:bg-chinese-black"
-                onClick={() => setIsDocsListOpen(!isDocsListOpen)}
-              >
-                {selectedDocs && (
-                  <p className="my-3 mx-4 overflow-hidden text-ellipsis whitespace-nowrap">
-                    {selectedDocs.name} {selectedDocs.version}
-                  </p>
-                )}
-                <img
-                  src={Arrow2}
-                  alt="arrow"
-                  className={`${!isDocsListOpen ? 'rotate-0' : 'rotate-180'
-                    } ml-auto mr-3 w-3 transition-all`}
-                />
-              </div>
+              <Dropdown
+                options={docs}
+                selectedDocs={selectedDocs}
+                setSelectedDocs={setSelectedDocs}
+                isDocsListOpen={isDocsListOpen}
+                setIsDocsListOpen={setIsDocsListOpen}
+                handleDeleteClick={handleDeleteClick}
+              />
               <img
                 className="mt-2 h-9 w-9 hover:cursor-pointer"
                 src={UploadIcon}
                 onClick={() => setUploadModalState('ACTIVE')}
               ></img>
-              {isDocsListOpen && (
-                <div className="absolute top-12 left-0 right-6 z-10 ml-2 mr-4 max-h-52 overflow-y-scroll bg-white dark:bg-chinese-black shadow-lg">
-                  {docs ? (
-                    docs.map((doc, index) => {
-                      if (doc.model === embeddingsName) {
-                        return (
-                          <div
-                            key={index}
-                            onClick={() => {
-                              dispatch(setSelectedDocs(doc));
-                              setIsDocsListOpen(false);
-                            }}
-                            className="flex h-10 w-full cursor-pointer items-center justify-between border-x-2 border-b-[1px] dark:border-purple-taupe hover:bg-gray-100 dark:hover:bg-purple-taupe"
-                          >
-                            <p className="ml-5 flex-1 overflow-hidden overflow-ellipsis whitespace-nowrap py-3">
-                              {doc.name} {doc.version}
-                            </p>
-                            {doc.location === 'local' && (
-                              <img
-                                src={Trash}
-                                alt="Delete"
-                                className="mr-4 h-4 w-4 cursor-pointer hover:opacity-50"
-                                id={`img-${index}`}
-                                onClick={(event) => {
-                                  event.stopPropagation();
-                                  handleDeleteClick(index, doc);
-                                }}
-                              />
-                            )}
-                          </div>
-                        );
-                      }
-                    })
-                  ) : (
-                    <div className="h-10 w-full cursor-pointer  border-b-[1px] dark:border-b-purple-taupe hover:bg-gray-100">
-                      <p className="ml-5 py-3">No default documentation.</p>
-                    </div>
-                  )}
-                </div>
-              )}
             </div>
             <p className="ml-6 mt-3 text-sm font-semibold">Source Docs</p>
           </div>
-          <div className="flex flex-col gap-2 border-b-[1px] dark:border-b-purple-taupe py-2">
+          <div className="flex flex-col gap-2 border-b-[1px] py-2 dark:border-b-purple-taupe">
             <NavLink
               to="/settings"
               className={({ isActive }) =>
-                `my-auto mx-4 flex h-9 cursor-pointer gap-4 rounded-3xl hover:bg-gray-100 dark:hover:bg-purple-taupe ${isActive ? 'bg-gray-3000 dark:bg-transparent' : ''
+                `my-auto mx-4 flex h-9 cursor-pointer gap-4 rounded-3xl hover:bg-gray-100 dark:hover:bg-purple-taupe ${
+                  isActive ? 'bg-gray-3000 dark:bg-transparent' : ''
                 }`
               }
             >
               <NavImage Light={SettingGear} Dark={SettingGearDark} />
-              <p className="my-auto text-sm text-eerie-black  dark:text-white">Settings</p>
+              <p className="my-auto text-sm text-eerie-black  dark:text-white">
+                Settings
+              </p>
             </NavLink>
           </div>
 
-          <div className="flex flex-col gap-2 border-b-[1.5px] dark:border-b-purple-taupe py-2">
+          <div className="flex flex-col gap-2 border-b-[1.5px] py-2 dark:border-b-purple-taupe">
             <NavLink
               to="/about"
               className={({ isActive }) =>
-                `my-auto mx-4 flex h-9 cursor-pointer gap-4 rounded-3xl hover:bg-gray-100 dark:hover:bg-purple-taupe ${isActive ? 'bg-gray-3000 dark:bg-purple-taupe' : ''
+                `my-auto mx-4 flex h-9 cursor-pointer gap-4 rounded-3xl hover:bg-gray-100 dark:hover:bg-purple-taupe ${
+                  isActive ? 'bg-gray-3000 dark:bg-purple-taupe' : ''
                 }`
               }
             >
@@ -388,31 +339,31 @@ export default function Navigation({ navOpen, setNavOpen }: NavigationProps) {
             >
               <NavImage Light={Discord} Dark={DiscordDark} />
               {/*  <img src={isDarkTheme ? DiscordDark : Discord} alt="discord-link" className="ml-2 w-5" /> */}
-              <p className="my-auto text-sm">
-                Visit our Discord
-              </p>
+              <p className="my-auto text-sm">Visit our Discord</p>
             </a>
 
             <a
               href="https://github.com/arc53/DocsGPT"
               target="_blank"
               rel="noreferrer"
-              className="mt-auto mx-4 flex h-9 cursor-pointer gap-4 rounded-3xl hover:bg-gray-100 dark:hover:bg-purple-taupe"
+              className="mx-4 mt-auto flex h-9 cursor-pointer gap-4 rounded-3xl hover:bg-gray-100 dark:hover:bg-purple-taupe"
             >
               <NavImage Light={Github} Dark={GithubDark} />
-              <p className="my-auto text-sm">
-                Visit our Github
-              </p>
+              <p className="my-auto text-sm">Visit our Github</p>
             </a>
           </div>
         </div>
       </div>
-      <div className="fixed z-10 h-16 w-full border-b-2 dark:border-b-purple-taupe bg-gray-50 dark:bg-chinese-black md:hidden">
+      <div className="fixed z-10 h-16 w-full border-b-2 bg-gray-50 dark:border-b-purple-taupe dark:bg-chinese-black md:hidden">
         <button
           className="mt-5 ml-6 h-6 w-6 md:hidden"
           onClick={() => setNavOpen(true)}
         >
-          <img src={isDarkTheme ? HamburgerDark : Hamburger} alt="menu toggle" className="w-7" />
+          <img
+            src={isDarkTheme ? HamburgerDark : Hamburger}
+            alt="menu toggle"
+            className="w-7"
+          />
         </button>
       </div>
       <SelectDocsModal
