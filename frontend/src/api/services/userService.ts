0c062a848573590bd6ded270790b56aaea2ce251
Siddhant Rai <siddhant.rai.5686@gmail.com>
2024-07-24 23:08:42 +0530

    enhancement: implement api client in remaining places

--- frontend/src/api/services/userService.ts
+++ frontend/src/api/services/userService.ts
@@ -2,6 +2,9 @@ import apiClient from '../client';
 import endpoints from '../endpoints';
 
 const userService = {
+  getDocs: (): Promise<any> => apiClient.get(endpoints.USER.DOCS),
+  checkDocs: (data: any): Promise<any> =>
+    apiClient.post(endpoints.USER.DOCS_CHECK, data),
   getAPIKeys: (): Promise<any> => apiClient.get(endpoints.USER.API_KEYS),
   createAPIKey: (data: any): Promise<any> =>
     apiClient.post(endpoints.USER.CREATE_API_KEY, data),
@@ -18,6 +21,8 @@ const userService = {
     apiClient.get(endpoints.USER.SINGLE_PROMPT(id)),
   deletePath: (docPath: string): Promise<any> =>
     apiClient.get(endpoints.USER.DELETE_PATH(docPath)),
+  getTaskStatus: (task_id: string): Promise<any> =>
+    apiClient.get(endpoints.USER.TASK_STATUS(task_id)),
 };
 
 export default userService;
