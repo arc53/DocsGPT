a83a56eecd8776986cba4681c06d672ed36fa430
ManishMadan2882 <manishmadan321@gmail.com>
2024-08-29 03:14:57 +0530

    update script

--- extensions/react-widget/publish.sh
+++ extensions/react-widget/publish.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-## chmod +x publish.sh   - to upgrade ownership
-# Exit immediately if a command exits with a non-zero status.
+## chmod +x publish.sh - to upgrade ownership
 set -e
-
-# Define the function to update package.json and publish the package
 cat package.json >> package_copy.json
 publish_package() {
   PACKAGE_NAME=$1
@@ -11,29 +8,29 @@ publish_package() {
   # Update package name in package.json
   jq --arg name "$PACKAGE_NAME" '.name=$name' package.json > temp.json && mv temp.json package.json
 
-  # Remove 'target' key if the package name is 'widget-react'
+  # Remove 'target' key if the package name is 'docsgpt-react'
   if [ "$PACKAGE_NAME" = "docsgpt-react" ]; then
     jq 'del(.targets)' package.json > temp.json && mv temp.json package.json
   fi
+
   if [ -d "dist" ]; then
     echo "Deleting existing dist directory..."
     rm -rf dist
   fi
-  # Increment version (patch by default)
-  #npm version patch
 
-  # Run the build command
+  npm version patch
+
   npm run "$BUILD_COMMAND"
 
   # Publish to npm
-  npm pack
+  npm publish
   echo "Published ${PACKAGE_NAME}"
 }
 
-# Publish widget package
+# Publish docsgpt package
 publish_package "docsgpt" "build"
 
-# Publish widget-react package
+# Publish docsgpt-react package
 publish_package "docsgpt-react" "build:react"
 
 # Clean up
