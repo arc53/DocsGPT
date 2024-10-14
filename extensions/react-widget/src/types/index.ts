0481e766ae420bbab3ed86b106448d3338952277
utin-francis-peter <utinfrancis.dev@gmail.com>
2024-10-14 12:30:57 +0100

    chore: updated Query and WidgetProps interface with source property

--- extensions/react-widget/src/types/index.ts
+++ extensions/react-widget/src/types/index.ts
@@ -1,16 +1,21 @@
 export type MESSAGE_TYPE = 'QUESTION' | 'ANSWER' | 'ERROR';
+
 export type Status = 'idle' | 'loading' | 'failed';
+
 export type FEEDBACK = 'LIKE' | 'DISLIKE';
+
 export type THEME = 'light' | 'dark';
+
 export interface Query {
   prompt: string;
   response?: string;
   feedback?: FEEDBACK;
   error?: string;
-  sources?: { title: string; text: string }[];
+  sources?: { title: string; text: string, source:string }[];
   conversationId?: string | null;
   title?: string | null;
 }
+
 export interface WidgetProps {
   apiHost?: string;
   apiKey?: string;
@@ -24,4 +29,5 @@ export interface WidgetProps {
   buttonIcon?:string;
   buttonBg?:string;
   collectFeedback?:boolean
+  showSources?: boolean
 }
