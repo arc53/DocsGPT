0c46f3c205d7b35e8f5b4e36af80b2815117dd25
sayanm16 <sayan.git.16@gmail.com>
2024-10-21 16:25:19 +0530

    Remove log

--- frontend/src/settings/APIKeys.tsx
+++ frontend/src/settings/APIKeys.tsx
@@ -42,7 +42,6 @@ export default function APIKeys() {
         return response.json();
       })
       .then((data) => {
-        console.log(data);
         data.success === true &&
           setApiKeys((previous) => previous.filter((elem) => elem.id !== id));
       })
