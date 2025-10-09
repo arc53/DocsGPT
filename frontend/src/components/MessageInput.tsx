96f8c1785d8cd1afd71735fb9829cde0179482b9
Rahul <rahulgithub96@gmail.com>
2025-10-09 02:01:25 -0700

    feat: Add button to cancel LLM response (#1978)
    * feat: Add button to cancel LLM response
    - Replace text area with cancel button when loading.
    - Add useEffect to change elipsis in cancel button text.
    - Add new SVG icon for cancel response.
    - Button colors match Figma designs.
    
    * fix: Cancel button UI matches new design
    - Delete cancel-response svg.
    - Change previous cancel button to match the new Figma design.
    - Remove console log in handleCancel function.
    
    * fix: Adjust cancel button rounding
    
    * feat: Update UI for send button
    - Add SendArrowIcon component, enables dynamic svg color changes
    - Replace original icon
    - Update colors and hover effects
    
    * (fix:send-button) minor blink in transition
    
    ---------
    
    Co-authored-by: Manish Madan <manishmadan321@gmail.com>

--- frontend/src/components/MessageInput.tsx
+++ frontend/src/components/MessageInput.tsx
@@ -11,6 +11,7 @@ import ClipIcon from '../assets/clip.svg';
 import DragFileUpload from '../assets/DragFileUpload.svg';
 import ExitIcon from '../assets/exit.svg';
 import SendArrowIcon from './SendArrowIcon';
+import SendArrowIcon from './SendArrowIcon';
 import SourceIcon from '../assets/source.svg';
 import DocumentationDark from '../assets/documentation-dark.svg';
 import ToolIcon from '../assets/tool.svg';
