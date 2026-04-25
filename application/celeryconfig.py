9c8ae9d54033c01f1838b69c79b2d67149b4bb97
Alex <a@tushynski.me>
2026-04-25 14:38:24 +0100

    feat: redbeat

--- application/celeryconfig.py
+++ application/celeryconfig.py
@@ -9,3 +9,8 @@ accept_content = ['json']
 
 # Autodiscover tasks
 imports = ('application.api.user.tasks',)
+
+beat_scheduler = "redbeat.RedBeatScheduler"
+redbeat_redis_url = broker_url
+redbeat_key_prefix = "redbeat:docsgpt:"
+redbeat_lock_timeout = 90
